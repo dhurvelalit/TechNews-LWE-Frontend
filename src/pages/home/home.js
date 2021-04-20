11f0a2bf22b20cd737f5879250f23621a36d7cbf
Ganesh Gitte <ganeshs.gitte911@gmail.com>
2021-04-20 23:30:17 +0530

    modify homepage and add dummy image in assets

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,91 +1,15 @@
 import { React, useState } from "react";
 import TechNewsAppBar from "../../components/appBar/appBar";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
-import NewsItems from "./newsItems";
-import "./home.css";
-
-const useStyles = makeStyles({
-  list: {
-    width: "75vw",
-  },
-});
+import NewsArticle from "./newsArticle";
+// import NewsItems from "./newsItems";
 
 const HomePage = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const func = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  const classes = useStyles();
-
-  const list = () => (
-    <div role="presentation">
-      <List className={classes.list}>
-        {[
-          "Inbox",
-          "Starred",
-          "Send email",
-          "Drafts",
-          "Inbox",
-          "Starred",
-          "Send email",
-          "Drafts",
-          "Inbox",
-          "Starred",
-          "Send email",
-          "Drafts",
-        ].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List className={classes.list}>
-        {[
-          "All mail",
-          "Trash",
-          "Spam",
-          "Inbox",
-          "Starred",
-          "Send email",
-          "Drafts",
-          "Inbox",
-          "Starred",
-          "Send email",
-          "Drafts",
-        ].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
   return (
     <div>
       <TechNewsAppBar drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
-      <NewsItems />
-      <div>
-        <Drawer open={drawerOpen} onClose={func}>
-          {list()}
-        </Drawer>
-      </div>
+      {/* <NewsItems /> */}
+      <NewsArticle />
     </div>
   );
 };
